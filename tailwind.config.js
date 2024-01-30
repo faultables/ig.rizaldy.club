4131ba6660c90d1a2846726e33fea495ba23d81a
rizaldy <oss@rizaldy.club>
2024-01-30 23:47:14 +0700

    chore: init

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  mode: "jit",
+  content: ["./pages/**/*.js", "./components/**/*.js"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
