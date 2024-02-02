9bb07798ed464fc6aa75ce22e2ff4ae0b57c3ae3
faultables <faultables@users.noreply.github.com>
2024-02-02 11:45:40 +0000

    deploy: be9140c4935c2328934a5c75371ec6531688cace

---
+++ _next/static/ByzAO7G-CauRUpjUuzjNb/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/pages/index-c91f1d8faf4d92e1.js"],"/_error":["static/chunks/pages/_error-b6491f42fb2263bb.js"],"/static/about":["static/chunks/pages/static/about-30f1ad42665a60b3.js"],sortedPages:["/","/_app","/_error","/static/about"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
